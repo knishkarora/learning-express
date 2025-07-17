b920933ee20529bc5722afb3394160267ad61aff
user.name like: "Knishk Arora <user.email like: "knishkarorapc77@gmail.cim>
2025-07-17 21:15:56 +0530

    user schema created

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const app = express();
 
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.get("/", (req, res) => {
-    res.send("home page");
+    res.render("index");
+    console.log("Home page accessed");
+})
+app.post("/submit", (req, res) => {
+    console.log(req.body);
+    res.send("Form submitted");
 })
 
 app.get("/about", (req, res) => {
